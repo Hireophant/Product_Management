80fc22f52fc2309e389cb86135cf7790fe584d2d
Hireophant <tntdanh2506@gmail.com>
2026-02-02 04:07:03 +0700

    fix photo

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -168,3 +168,20 @@ if (buttonDelete.length > 0) {
 }
 
 //End Delete Product
+
+
+// Upload Image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+    const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+    const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+
+    uploadImageInput.addEventListener("change", (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            uploadImagePreview.src = URL.createObjectURL(file);
+        }
+    });
+}
+
+// End Upload Image
