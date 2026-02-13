e3318eafc1c1b04fe6ebac35e08a7fe3e2988fe4
Hireophant <tntdanh2506@gmail.com>
2026-02-14 04:42:14 +0700

    Show new products

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -8,10 +8,22 @@ module.exports.index = async (req, res) => {
     featured: "1",
     deleted: false,
     status: "active",
-  });
+  }).limit(6);
   const newProducts = productsHelper.priceNewProducts(productsFeatured);
+  //Hết lấy sản phẩm nổi bật
+
+  //Hiển thị danh sách sản phẩm mới nhất
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
+  //Hết hiển thị danh sách sản phẩm mới nhất
   res.render("client/pages/home/index", {
     pageTitle: "Trang Chủ",
     productsFeatured: newProducts,
+    productsNew: newProductsNew,
   });
 };
