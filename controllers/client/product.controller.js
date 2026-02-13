4ad947405c1f4849e1ebc419a269a30203b90196
Hireophant <tntdanh2506@gmail.com>
2026-02-14 05:31:06 +0700

    page list products in client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require("../../models/product.model");
 const productsHelper = require("../../helpers/products");
+const ProductCategory = require("../../models/product-category.model");
+const productsCategoryHelper = require("../../helpers/products-category");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -33,4 +35,32 @@ module.exports.detail = async (req, res) => {
     req.flash("error", "Không tìm thấy sản phẩm!");
     res.redirect(`/products`);
   }
+};
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const slugCategory = req.params.slugCategory;
+
+  const category = await ProductCategory.findOne({
+    slug: slugCategory,
+    deleted: false,
+    status: "active",
+  });
+
+  const listSubCategory = await productsCategoryHelper.getSubCategory(
+    category.id,
+  );
+  const listSubCategoryId = listSubCategory.map((item) => item.id);
+
+  const products = await Product.find({
+    products_category_id: { $in: [category.id, ...listSubCategoryId] },
+    deleted: false,
+    status: "active",
+  }).sort({ position: "desc" });
+
+  const newProducts = productsHelper.priceNewProducts(products);
+  res.render("client/pages/products/index", {
+    pageTitle: category.title,
+    products: newProducts,
+  });
 };
--- routes/clients/product.route.js
+++ routes/clients/product.route.js
@@ -2,10 +2,8 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/client/product.controller");
 
-
-
 router.get("/", controller.index);
-router.get("/:slug", controller.detail);
-
+router.get("/:slugCategory", controller.category);
+//router.get("/:slug", controller.detail);
 
 module.exports = router;
