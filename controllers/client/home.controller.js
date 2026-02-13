0cd7d391a3bb601987fe99b4a91414557d88a722
Hireophant <tntdanh2506@gmail.com>
2026-02-14 04:29:58 +0700

    special product at client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,17 @@
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
+
 //[Get] /home
 module.exports.index = async (req, res) => {
+  //Lấy ra sản phẩm nổi bật
+  const productsFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active",
+  });
+  const newProducts = productsHelper.priceNewProducts(productsFeatured);
   res.render("client/pages/home/index", {
     pageTitle: "Trang Chủ",
+    productsFeatured: newProducts,
   });
 };
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,38 +1,36 @@
 const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
-        status: "active",
-        deleted: false
-    }).sort({ position: "desc" });
+  const products = await Product.find({
+    status: "active",
+    deleted: false,
+  }).sort({ position: "desc" });
 
-    const newProducts = products.map(item => {
-        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0);
-        return item;
-    })
-    res.render("client/pages/products/index", {
-        pageTitle: "Danh Sách Sản Phẩm",
-        products: newProducts
-    });
+  const newProducts = productsHelper.priceNewProducts(products);
+  res.render("client/pages/products/index", {
+    pageTitle: "Danh Sách Sản Phẩm",
+    products: newProducts,
+  });
 };
 
 // [GET] /products/:slug
 module.exports.detail = async (req, res) => {
-    try{
-        const slug = req.params.slug;
-        const find = {
-            slug: slug,
-            deleted: false,
-            status: "active",
-        };
-        const product = await Product.findOne(find);
-        res.render("client/pages/products/detail", {
-            pageTitle: product.title,
-            product: product,
-        });
-    } catch (error) {
-        req.flash("error", "Không tìm thấy sản phẩm!");
-        res.redirect(`/products`);
-    }
+  try {
+    const slug = req.params.slug;
+    const find = {
+      slug: slug,
+      deleted: false,
+      status: "active",
+    };
+    const product = await Product.findOne(find);
+    res.render("client/pages/products/detail", {
+      pageTitle: product.title,
+      product: product,
+    });
+  } catch (error) {
+    req.flash("error", "Không tìm thấy sản phẩm!");
+    res.redirect(`/products`);
+  }
 };
