953b0eeaaf5aa27f27dc57dc80fbe7c1dda8b93c
Hireophant <tntdanh2506@gmail.com>
2026-02-14 15:34:02 +0700

    page search products

--- routes/clients/index.route.js
+++ routes/clients/index.route.js
@@ -1,8 +1,10 @@
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
 const categoryMiddleware = require("../../middleware/client/category.middleware");
+const searchRoutes = require("./search.route");
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
   app.use("/", homeRoutes);
   app.use("/products", productRoutes);
+  app.use("/search", searchRoutes);
 };
