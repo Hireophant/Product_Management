a83b3c799a2bdf9af7b49c06c7a10dc8d8c7832a
Hireophant <tntdanh2506@gmail.com>
2026-02-15 18:23:48 +0700

    page cart

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,31 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
+//[Get] /cart
+module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const cart = await Cart.findOne({ _id: cartId });
+
+  if (cart.products.length > 0) {
+    for (const item of cart.products) {
+      const productId = item.product_id;
+      const productInfo = await Product.findOne({ _id: productId });
+      productInfo.priceNew = productsHelper.priceNewProduct(productInfo);
+      item.productInfo = productInfo;
+      item.totalPrice = item.quantity * productInfo.priceNew;
+    }
+  }
+
+  cart.totalPrice = cart.products.reduce(
+    (total, item) => total + item.totalPrice,
+    0,
+  );
+
+  res.render("client/pages/cart/index", {
+    pageTitle: "Giỏ Hàng",
+    cartDetail: cart,
+  });
+};
 
 //[Post] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
