323e1b31570e5a332948bd67effff3dfb8646add
Hireophant <tntdanh2506@gmail.com>
2026-02-16 06:04:09 +0700

    page register

--- routes/clients/index.route.js
+++ routes/clients/index.route.js
@@ -5,6 +5,7 @@ const searchRoutes = require("./search.route");
 const cartMiddleware = require("../../middleware/client/cart.middleware");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
+const userRoutes = require("./user.route");
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
   app.use(cartMiddleware.cartId);
@@ -13,4 +14,5 @@ module.exports = (app) => {
   app.use("/search", searchRoutes);
   app.use("/cart", cartRoutes);
   app.use("/checkout", checkoutRoutes);
+  app.use("/user", userRoutes);
 };
