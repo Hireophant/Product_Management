c36c1899a6b541748eebbade543e449ff897ace2
Hireophant <tntdanh2506@gmail.com>
2026-02-22 19:03:41 +0700

    add multi images (continue)

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,44 +1,12 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
 const uploadToCloudinary = require("../../helpers/uploadToCloudinary");
+const chatSocket = require("../../sockets/client/chat.socket");
 //[Get] /chat
 module.exports.index = async (req, res) => {
-  const userId = res.locals.user.id;
-  const fullName = res.locals.user.fullName;
-
   //SocketIO
-  _io.once("connection", (socket) => {
-    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
-      let images = [];
-      for (const imageBuffer of data.images) {
-        const link = await uploadToCloudinary(imageBuffer);
-        images.push(link);
-      }
-      //Lưu vào database
-      const chat = new Chat({
-        user_id: userId,
-        content: data.content,
-        images: images,
-      });
-      await chat.save();
+  chatSocket(res);
 
-      //Trả data cho client
-      _io.emit("SERVER_RETURN_MESSAGE", {
-        userId: userId,
-        fullName: fullName,
-        content: data.content,
-        images: data.images,
-      });
-    });
-
-    socket.on("CLIENT_SEND_TYPING", (type) => {
-      socket.broadcast.emit("SERVER_RETURN_TYPING", {
-        userId: userId,
-        fullName: fullName,
-        type: type,
-      });
-    });
-  });
   //End SocketIO
   //Lấy ra data
   const chats = await Chat.find({
