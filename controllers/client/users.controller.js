4e2ccf253bde108ac2009160b6b1cbbef7ed4a25
Hireophant <tntdanh2506@gmail.com>
2026-02-23 04:49:40 +0700

    add frined request

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -1,12 +1,22 @@
 const User = require("../../models/user.model");
-
+const usersSocket = require("../../sockets/client/users.socket");
 // [GET] /users/not-friend
 module.exports.notFriend = async (req, res) => {
+  //Socket
+  usersSocket(res);
+  //End Socket
   const userId = res.locals.user.id;
+  const myUser = await User.findOne({ _id: userId });
+  const requestFriends = myUser.requestFriends;
+  const acceptFriends = myUser.acceptFriends;
   const users = await User.find({
     status: "active",
     deleted: false,
-    _id: { $ne: userId },
+    $and: [
+      { _id: { $ne: userId } },
+      { _id: { $nin: requestFriends } },
+      { _id: { $nin: acceptFriends } },
+    ],
   }).select("fullName avatar");
   res.render("client/pages/users/not-friend", {
     pageTitle: "Danh sách người dùng",
